b4f0d6e726dd3d86076fdb38650100bbb78b897a
Popa Tijana <tijanapopa@yahoo.com>
2025-05-07 00:35:19 +0300

    Detalii proiect, phase 1+2

--- treasure_hub.c
+++ treasure_hub.c
@@ -69,7 +69,7 @@ void verificare(int sig)
 }
 void sendcommand(const char *cc)
 {
-  if(strcmp(cc, "--list_hunt")==0)
+  if(strcmp(cc, "--list_hunts")==0)
     {
       write_intxt(cc);
       if(kill(monitor_pid, SIGUSR1)==-1)
@@ -128,31 +128,32 @@ int main()
   char buff[Max],buff2[Max], comprim[MAX], buf3[70];
   while(1)
  {
+   printf("Introduceti comenzile corespunzatoare\n\n");
     fgets(buff,Max, stdin);
     buff[strcspn(buff, "\n")]='\0';
     if(command(buff)==1)
       {printf("Se verificam daca monitorul este pornit pentru inceput\n");
-         if(monitor_execution==0 &&  monitor_pid==-1 )
-        	{  printf("Monitorul era/este pornit!\n");
-        monitor_pid=fork();
-        if(monitor_pid<0)
-	   {
-	     perror("Eroare la fork : ");
-	     exit(-1); 
-	   }
-	 monitor_execution=1;
-          if(monitor_pid==0)
+
+	if(monitor_execution==1 &&  monitor_pid>0 )
         	{
-	           execl("./monitor", "./monitor",NULL);
-		   /// perror("eroare pentru execl\n");
-	           exit(0);
-        	}
-	   printf("Monitorul functioneaza cu acest pid %d\n",monitor_pid);
-         }
+		  printf("Monitorul functioneaza cu acest pid %d\n",monitor_pid);		  
+		}
 	 else
 	   {
-	           printf("Monitorul s a oprit!\n");
-	            exit(1);
+	            printf("Monitorul era/este pornit!\n");
+		  monitor_pid=fork();
+		  if(monitor_pid<0)
+		    {
+		      perror("Eroare la fork : ");
+		      exit(-1); 
+		    }
+		  monitor_execution=1;
+		  if(monitor_pid==0)
+		    {
+		      execl("./monitor", "./monitor",NULL);
+		      /// perror("eroare pentru execl\n");
+		      exit(0);
+		    }
 	   }
       }
     else
@@ -171,17 +172,17 @@ int main()
 	      printf("Introduceti numele huntului \n");
               fgets(buff2,Max,stdin);
               buff2[strcspn(buff2,"\n")]='\0';
-              snprintf(comprim, MAX,"--list_treasures");
+              snprintf(comprim, MAX,"--list_treasures %s", buff2);
               sendcommand(comprim);
               break;
             case 4:printf("S-a optat pentru vizualizarea treasure-ului specificat \n");
               printf("Introduceti pentru inceput hunt-ul\n");
               fgets(buff2,Max,stdin);
               buff2[strcspn(buff2,"\n")]='\0';
-              printf("Introduceti aum treasure-ul cautat \n");
+              printf("Introduceti acum treasure-ul cautat \n");
               fgets(buf3,70,stdin);
               buf3[strcspn(buf3,"\n")]='\0';
-              snprintf(comprim, MAX,"--view_treasure");
+              snprintf(comprim, MAX,"--view_treasure %s",buf3);
               sendcommand(comprim);
            break;
            case 5:printf("S-a optat pentru oprirea monitorului \n");
