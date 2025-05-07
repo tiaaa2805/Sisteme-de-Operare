aac6bed07cf893e8ce7e3cf55a6813d06876a050
Popa Tijana <tijanapopa@yahoo.com>
2025-05-07 14:23:30 +0300

    sa vedem

--- monitor.c
+++ monitor.c
@@ -9,6 +9,8 @@
 #include<time.h>
 #include<dirent.h>
 #include<signal.h>
+#include<limits.h>
+#include<sys/wait.h>
 #include<stdbool.h>
 #include"treasur.h"
 #define Max 500
@@ -21,10 +23,10 @@ char  *readdetails()
   if(fd==-1)
     {
       perror("Eroare la deschiderea firierului pentru citirea comenzilor\n");
-      return;
+      return NULL;
     }
   lseek(fd,ls*sizeof(char),SEEK_SET);
-   char linie[256];
+  char *linie=malloc(Max);
    int i=0;
   while(read(fd,linie,1)==1)
     {
@@ -49,10 +51,10 @@ char  *readdetails()
 void handler_view_treasure()
 {
   char op[Max], hunt[Max], tres[Max];
-  op=readdetails();
+  strcpy( op,readdetails());
   int len=strcspn(op," ");
   int len2=strcspn(op+len+1," ");
-  strncpy(hunt,op+len+1,streln(op)-len2);
+  strncpy(hunt,op+len+1,strlen(op)-len2);
   strcpy(tres,op+len2+1);
   if(monitor_stop==0)
     view(hunt,tres);
@@ -62,7 +64,7 @@ void handler_view_treasure()
 void handler_list_treasures()
 {
   char op[Max];
-  op=readdetails();
+  snprintf(op,Max,"%s",readdetails());
   int len=strcspn(op," ");
   if(monitor_stop==0)
     list(op+len+1);
@@ -71,7 +73,8 @@ void handler_list_treasures()
 }
 
 void handler_stop_monitor()
-{
+{printf("Monitorul se va inchide in 5 secunde\n");
+  
     usleep(5000000);
     printf("Monitorul se afla in procesul de inchidere\n");
     monitor_stop=-1;
@@ -85,40 +88,45 @@ void handler_sigc()
     {
       printf("Eroare la waitpid\n");
     }
-  printf("Monitorul are acest status %d\n", WEXISTATUS(status));
+  printf("Monitorul are acest status %d\n", WEXITSTATUS(status));
+}
+int este_director(const char *oo)
+{
+  struct stat st;
+  return ((stat(oo,&st)==0) && S_ISDIR(st.st_mode));
 }
 void handler_list_directory()
 {
-  char cale[Max];
+  char cale[PATH_MAX], director[Max];
   DIR *fd=opendir(".");
   struct dirent *citim;
   if(fd)
     {
-      while((citim==readdir(fd))!=NULL)
+      while((citim=readdir(fd))!=NULL)
 	{
 	  if(strcmp(citim->d_name,".")==0 || strcmp(citim->d_name,"..")==0)
 	    continue;
-	  if(strcmp(citim->d_type, DT_DIR)==0)
+	  
+	  if(este_director(citim->d_name))
 	    {
-	      strcpy(director,citim);
+	      snprintf(director,Max,"%s",citim->d_name);
 	      DIR *fdd=opendir(".");
 	      struct dirent *citim2;
 	      if(fdd)
 		{
-		  while((citim2==readdir(fdd))!=NULL)
+		  while((citim2=readdir(fdd))!=NULL)
 		    {
-		      if(strcmp(citim2->d_nume,".")==0 || strcmp(citim2->d_nume,"..")==0)
+		      if(strcmp(citim2->d_name,".")==0 || strcmp(citim2->d_name,"..")==0)
 			continue;
-		      printf("Numele huntului este %s\n",citim2->d_nume);
-		      if(strstr(citim2->d_nume,".dat")!=NULL)
-			{
-			   snprintf(cale,Max,"%s/%s",director,citim2->d_nume);
+		      printf("Numele huntului este %s\n",citim2->d_name);
+		      
+			  snprintf(cale,sizeof(cale),"%s/%s",citim2->d_name,"treasure.dat");
 		           int fisier=open(cale, O_RDONLY,S_IRUSR);
 		           int nr=nrtreasure(fisier);
 		           printf("Numarul treasurilor este %d \n\n", nr);
 		           close(fisier);
 			   break;
-			}
+			
 		      
 		    }
 		  
@@ -135,6 +143,8 @@ void handler_list_directory()
     printf("Directorul a fost inchis cu succes \n");
   else
     perror("INCHIDEREA NU A FUNCTIONAT\n");
+
+  
 }
 void setup_for_handler(int sig)
 {
@@ -142,21 +152,23 @@ void setup_for_handler(int sig)
     {
     case SIGUSR1:printf("Ne ocupam de listarea directoarelor si numarului de treasururi in fiecare hunt\n");
       handler_list_directory();
+        printf("\n\n--------------------------------------------------\n\n");
       break;
     case SIGUSR2:
       printf("Ne ocupam de listarea treasure_urilor dintr-un hunt\n");
       handler_list_treasures();
+       printf("\n\n--------------------------------------------------\n\n");
       break;
     case SIGTERM :
       printf("Ne ocupam de inchiderea monitorului\n");
       handler_stop_monitor();
+       printf("\n\n--------------------------------------------------\n\n");
       break;
     case SIGINT:
       printf("Ne ocupam de deschiderea unui HUNT, cu scopul analizei unui treasure\n");
       handler_view_treasure();
-      break;
-    case SIGCHLD:
-      /* break;
+       printf("\n\n--------------------------------------------------\n\n");
+      /*break;
     case SIGHUP:///pt remove
       */
     }
@@ -191,6 +203,11 @@ int main()
     {
       perror("Eroare la sigaction\n");
       exit(1);
+    }
+  printf("Pidul monitorului nostru este %d\n\n",getpid());
+  while(1)
+    {
+      pause();
     }
   return 0;
 }
